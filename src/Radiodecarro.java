1b2199128bb6e75901be39105b510eef554efc93
Luisa2125 <luiyi2125@gmail.com>
2015-07-14 22:07:42 -0600

    Hojaparte 3

--- src/Radiodecarro.java
+++ src/Radiodecarro.java
@@ -29,11 +29,56 @@ public class Radiodecarro implements Radio {
         FM=87.9f;
     }
     
-    public void setEstado(boolean nEstado){}
-    public boolean getEstado(){}
-    public void setAMFM(int banda){}
-    public int getAMFM(){}
-    public int sincronizar(boolean ud){}
+    public void setEstado(boolean nEstado){
+        estado=nEstado;
+    }
+    public boolean getEstado(){
+        return estado;
+    }
+    public void setAMFM(int banda){
+        if(AMFM==0){
+            AM=estacion;
+        }
+        else{
+            FM=estacion;
+        }
+        AMFM=banda;
+        if(AMFM==0){
+            intervalo=10.00f;
+            estacion=AM;
+        }
+        else{
+            intervalo=0.2f;
+            estacion=FM;
+        }
+    }
+    public int getAMFM(){
+        return AMFM;
+    }
+    public int sincronizar(boolean ud){
+        if(ud){
+            estacion+=intervalo;
+        }
+        else{
+            estacion-=intervalo;
+        }
+        if(AMFM==0){
+            if(estacion>1610){
+                estacion=530;
+            }
+            else if(estacion<530){
+                estacion=1610;
+            }
+        }
+        else{
+            if(estacion>107.9){
+                estacion=87.9f;
+            }
+            else if(estacion<87.9){
+                estacion=107.9f;
+            }
+        }
+    }
     public void guardar(int pos){}
     public void memoria(int pos){}
     public float getEmisora(){}
