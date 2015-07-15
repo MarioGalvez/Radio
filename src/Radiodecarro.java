c2782687b062272aa93cf3334fd1acfe11d3c6ac
Luisa2125 <luiyi2125@gmail.com>
2015-07-14 21:13:14 -0600

    Hojaparte 1

---
+++ src/Radiodecarro.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luiyi
+ */
+public class Radiodecarro implements Radio {
+    private boolean estado;
+    private int AMFM;
+    private float[] botonesAM;
+    private float[] botonesFM;
+    private float estacion;
+    private float intervalo;
+    private float AM;
+    private float FM;
+}
