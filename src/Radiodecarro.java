5f8fcab41abb85a3f4c783313fd601f5a1b8f98a
Luisa2125 <luiyi2125@gmail.com>
2015-07-14 21:27:03 -0600

    Hojaparte 2

--- src/Radiodecarro.java
+++ src/Radiodecarro.java
@@ -17,4 +17,26 @@ public class Radiodecarro implements Radio {
     private float intervalo;
     private float AM;
     private float FM;
+    
+    public Radiocarro(){
+        estado=false;
+        AMFM=0;
+        botonesAM=new float[12];
+        botonesFM=new float[12];
+        estacion=530.00f;
+        intervalo=10.00f;
+        AM=530.00f;
+        FM=87.9f;
+    }
+    
+    public void setEstado(boolean nEstado){}
+    public boolean getEstado(){}
+    public void setAMFM(int banda){}
+    public int getAMFM(){}
+    public int sincronizar(boolean ud){}
+    public void guardar(int pos){}
+    public void memoria(int pos){}
+    public float getEmisora(){}
+    
+    
 }
